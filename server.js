18aa10aed65f8dda494e9c24b3768ad7603cae98
fadiHB <eng.fadi.hb@gmail.com>
2020-10-24 15:26:01 +0300

    part1

---
+++ server.js
@@ -0,0 +1,15 @@
+let express = require('express'); // libraru for the server
+let port = 3000;
+let app = express();
+app.use(express.static('public'))
+
+// test from localhost:3000
+app.get('/',(req,res) => {
+    res.status(200).sendFile('index.html')
+})
+
+// test from ubunto
+app.listen(port,() => {
+    console.log('test');
+})
+
